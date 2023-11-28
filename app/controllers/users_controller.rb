a6903d2fdeab5b28bf8e4b85db7c4bfc923adccc
Samuel-Lubliner <samlubliner@gmail.com>
2023-11-28 17:23:35 -0600

    Add user idex with search

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,5 +5,10 @@ class UsersController < ApplicationController
     @q = Availability.ransack(params[:q])
     @availabilities = @q.result.includes(:user, event_requests: :user)
   end
+
+  def index
+    @q = User.ransack(params[:q])
+    @users = @q.result(distinct: true)
+  end
     
 end
