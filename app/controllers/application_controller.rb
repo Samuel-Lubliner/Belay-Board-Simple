e0e4441657fdfb4602b220839adf17e6b6a5e00d
Samuel-Lubliner <samlubliner@gmail.com>
2023-12-06 14:52:43 -0600

    Add is_public to new devise reg

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -16,7 +16,7 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:username])
-    devise_parameter_sanitizer.permit(:account_update, keys: [:username])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :is_public])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:username, :is_public])
   end
 end
