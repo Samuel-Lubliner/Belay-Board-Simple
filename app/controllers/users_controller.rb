dfac7cb49e4b18f1ed58a4e6e267c7e0963a260c
Samuel-Lubliner <samlubliner@gmail.com>
2023-12-08 15:39:52 -0600

    Add charts for partners and climb time

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,25 +2,88 @@ class UsersController < ApplicationController
   before_action :authenticate_user!
 
   def dashboard
-    @climbers_data = {
-      'Beginner' => Climber.where(beginner: true).count,
-      'Intermediate' => Climber.where(intermediate: true).count,
-      'Advanced' => Climber.where(advanced: true).count,
 
-    }
+    @availability_category_data = calculate_availability_categories
 
-    @availability_data = {
-      'Boulder' => Availability.where(boulder: true).count,
-      'Top Rope' => Availability.where(top_rope: true).count,
-      'Lead' => Availability.where(lead: true).count
-    }
+    @climbing_time_data = calculate_climbing_time
 
-    @climbing_styles_data = {
-      'Boulder' => Climber.where(boulder: true).count,
-      'Top Rope' => Climber.where(top_rope: true).count,
-      'Lead' => Climber.where(lead: true).count
-    }
-
+    @climbing_partners_data = calculate_climbing_partners
 
   end
+
+  private
+
+  def calculate_availability_categories
+    user_availabilities = Availability.where(user_id: current_user.id)
+  
+    friend_ids = FriendRequest.where(receiver_id: current_user.id, status: 'accepted').pluck(:sender_id) +
+                 FriendRequest.where(sender_id: current_user.id, status: 'accepted').pluck(:receiver_id)
+    friend_availabilities = Availability.where(user_id: friend_ids)
+  
+    # Fetch availabilities where the current user is accepted in event requests
+    accepted_event_availabilities = EventRequest.where(user_id: current_user.id, status: 'accepted')
+                                                .includes(:availability)
+                                                .map(&:availability)
+  
+    # Combine all relevant availabilities
+    all_relevant_availabilities = (user_availabilities + friend_availabilities + accepted_event_availabilities).uniq
+  
+    categories = %w[boulder lead top_rope]
+    categories.each_with_object({}) do |category, data|
+      data[category.capitalize] = all_relevant_availabilities.count { |availability| availability[category] }
+    end
+  end
+  
+  
+
+  def calculate_climbing_time
+    user_availabilities = Availability.where(user_id: current_user.id)
+    accepted_event_requests = EventRequest.where(user_id: current_user.id, status: 'accepted')
+                                          .includes(:availability)
+                                          .map(&:availability)
+  
+    all_relevant_availabilities = (user_availabilities + accepted_event_requests).uniq
+  
+    # Determine the date range from availabilities
+    earliest_date = Availability.minimum(:start_time).to_date
+    latest_date = Availability.maximum(:end_time).to_date
+    date_range = (earliest_date..latest_date).to_a
+  
+    # Initialize climbing time data for each date in the range
+    climbing_time_data = date_range.each_with_object({}) { |date, data| data[date] = 0 }
+  
+    # Aggregate climbing time
+    all_relevant_availabilities.each do |availability|
+      date = availability.start_time.to_date
+      if climbing_time_data.key?(date)
+        duration = availability.end_time - availability.start_time
+        climbing_time_data[date] += duration
+      end
+    end
+  
+    climbing_time_data.transform_values { |seconds| seconds / 3600.0 } # Convert seconds to hours
+  end
+  
+
+  def calculate_climbing_partners
+    # Total time for availabilities created by the current user
+    user_availabilities_with_others = EventRequest.where(availability_id: Availability.where(user_id: current_user.id).select(:id), status: 'accepted')
+                                                   .joins(:availability)
+                                                   .group(:user_id)
+                                                   .sum('EXTRACT(EPOCH FROM (availabilities.end_time - availabilities.start_time))')
+  
+    # Total time for availabilities where the current user is accepted
+    availabilities_with_current_user = EventRequest.joins(:availability)
+                                                   .where(user_id: current_user.id, status: 'accepted')
+                                                   .group('availabilities.user_id')
+                                                   .sum('EXTRACT(EPOCH FROM (availabilities.end_time - availabilities.start_time))')
+  
+    # Combine the total times, summing up the durations
+    combined_data = user_availabilities_with_others.merge(availabilities_with_current_user) { |key, a, b| a + b }
+  
+    # Transform user IDs to usernames and convert seconds to hours
+    combined_data.transform_keys { |user_id| User.find(user_id).username }
+                 .transform_values { |seconds| seconds / 3600.0 } # Convert seconds to hours
+  end
+  
 end
--- lib/tasks/dev.rake
+++ lib/tasks/dev.rake
@@ -15,17 +15,17 @@ if Rails.env.development?
     
       user_names = ["Sam", "Ben", "Olivia", "Rashid", "Robbie", "Julia"]
     
-      15.times do
+      10.times do
         fake_name = Faker::Name.unique.first_name
         user_names << fake_name
       end
     
       user_names.each do |name|
         user_attrs = {
-          username: name.downcase,
+          username: name,
           email: "#{name.downcase}@example.com",
           password: "password",
-          is_public: [true, false].sample # Randomly assign is_public
+          is_public: [true, false].sample
         }
         puts "Creating user with attributes: #{user_attrs.inspect}"
     
@@ -57,23 +57,57 @@ if Rails.env.development?
     
     task add_availabilities: :environment do
       puts "Adding availabilities..."
-      event_names = ["Boulder", "Top Rope", "Lead Climb", "Train"]
+      event_names = ["Boulder", "Top Rope", "Lead Climb", "Train", "Social Climb", "Climbing Team", "Meet up", "Birthday Party", "Fun Climb", "Climb With Me"]
+      locations = ["Movement LP", "Movement Wrigley", "FA Avondale", "FA Loop", "BKB West Loop"]
+    
       User.find_each do |user|
         (0...2).each do |month|
           start_date = Date.today.beginning_of_month + month.months
           end_date = start_date.end_of_month
     
           (start_date..end_date).each do |day|
-            start_time = Faker::Time.between_dates(from: day, to: day, period: :day)
+            morning = day.to_time.change(hour: 6)
+            evening = day.to_time.change(hour: 21)
+            start_time = Faker::Time.between(from: morning, to: evening)
             end_time = start_time + [2, 3, 4].sample.hours
     
-            event_name = event_names.sample
+            # Ensure end_time does not exceed 9 PM
+            if end_time.hour > 21 || (end_time.hour == 21 && end_time.min > 0)
+              end_time = day.to_time.change(hour: 21)
+            end
     
-            Availability.create!(event_name: event_name, start_time: start_time, end_time: end_time, user_id: user.id)
+            event_name = event_names.sample
+            location = locations.sample
+            description = Faker::Lorem.sentence
+    
+            Availability.create!(
+              event_name: event_name, 
+              start_time: start_time, 
+              end_time: end_time, 
+              user_id: user.id,
+              advanced: [true, false].sample,
+              beginner: [true, false].sample,
+              boulder: [true, false].sample,
+              indoor: [true, false].sample,
+              intermediate: [true, false].sample,
+              lead: [true, false].sample,
+              outdoor: [true, false].sample,
+              overhang: [true, false].sample,
+              slab: [true, false].sample,
+              sport: [true, false].sample,
+              top_rope: [true, false].sample,
+              trad: [true, false].sample,
+              vertical: [true, false].sample,
+              learn: [true, false].sample,
+              location: location,
+              description: description
+            )
           end
         end
       end
     end
+    
+    
 
     task add_event_requests: :environment do
       puts "Adding event requests..."
@@ -103,32 +137,28 @@ if Rails.env.development?
           end
         end
       end
+     puts "Comments added"
+    end
 
-
-    puts "Comments added"
-  end
-
-  task add_friend_requests: :environment do
+    task add_friend_requests: :environment do
     puts "Adding friend requests..."
   
     User.where(is_public: false).find_each do |private_user|
       # Select random users to send friend requests to the private user
       senders = User.where.not(id: private_user.id).order("RANDOM()").limit(5)
   
-      senders.each do |sender|
-        FriendRequest.create!(
-          sender: sender,
-          receiver: private_user,
-          status: 'pending' # Default status is 'pending'
-        )
-        puts "Friend request sent from #{sender.username} to #{private_user.username}"
+        senders.each do |sender|
+          FriendRequest.create!(
+            sender: sender,
+            receiver: private_user,
+            status: 'pending' # Default status is 'pending'
+          )
+          puts "Friend request sent from #{sender.username} to #{private_user.username}"
+        end
       end
+      puts "Friend requests added"
     end
-  
-    puts "Friend requests added"
-  end
 
-  
-  
-end
+    
+  end
 end
