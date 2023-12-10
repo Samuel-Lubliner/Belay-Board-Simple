3f780205ad60da3ea3f51aa8cc90a7ab5bf3324c
Samuel-Lubliner <samlubliner@gmail.com>
2023-12-09 21:54:07 -0600

    Refactor user mailer

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -7,7 +7,7 @@ class UserMailer < ApplicationMailer
   #
   def summary
     user = params[:user]
-    @greeting = "Hi #{user.username}"
+    @greeting = "Hi #{user.email}"
 
     # Fetch avaiabilities created by the user for the next day
     @user_availabilities = Availability.where(user_id: user.id)
@@ -17,6 +17,12 @@ class UserMailer < ApplicationMailer
     @accepted_availabilities = Availability.joins(:event_requests)
                                            .where(event_requests: { user_id: user.id, status: 'accepted' })
                                            .where('start_time >= ? AND start_time < ?', Date.tomorrow.beginning_of_day, Date.tomorrow.end_of_day)
+
+    # Fetch event requests with usernames grouped by status
+    @event_requests_by_status = EventRequest.where(availability_id: @user_availabilities.select(:id))
+                                            .joins(:user)
+                                            .group_by(&:status)
+                                            .transform_values { |requests| requests.map { |req| req.user.username } }
 
     mail to: user.email, subject: "Your Next Day's Availabilities Summary"
   end
